b03df54663218be060e22582814ef83a21094f7c
Sandhya <sandhya.k@tringapps.net>
2024-03-12 15:27:04 +0530

    App.test.js
    Updated the test method

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const linkElement = screen.getByText(/Remember/i);
   expect(linkElement).toBeInTheDocument();
 });
