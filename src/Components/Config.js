dab70250eb2a848eac4a51be12c1bdfb9fae9ced
Siva-bharathiP <136304492+Siva-bharathiP@users.noreply.github.com>
2024-03-26 08:15:45 +0530

    Update config.js

--- src/Components/Config.js
+++ src/Components/Config.js
@@ -1,4 +1,9 @@
 // export const PortURL = 'http://14.194.141.107:3002'; 
 
-export const PortURL = 'http://localhost:3001'; 
+// export const PortURL = 'http://14.194.141.107:3002'; 
+
+//export const PortURL = 'http://192.168.1.50:3001';
+
+export const PortURL = 'http://192.168.1.225:3001';
+
 
