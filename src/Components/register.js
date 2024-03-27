56b7b14c4048db1d8bd0d8ba849a812c106f2452
JohnsonMelvin <jmelvinmelu113@gmail.com>
2024-03-27 17:40:43 +0530

    Updated Login and Register

--- src/Components/register.js
+++ src/Components/register.js
@@ -1,54 +1,46 @@
 import React, { useState } from 'react';
-import { Container, Form, Button} from 'react-bootstrap';
+import { Container, Form, Button, Row, Col} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/register.css';
 import { TextField, InputAdornment } from '@mui/material';
-// import VisibilityIcon from '@mui/icons-material/Visibility';
-// import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { PortURL } from './Config';
 import Header from './Header';
 
-
 function Register() {
-  const [username, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [company, setCompany] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState('');
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [usernameError, setUsernameError] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [companyError, setCompanyError] = useState('');
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
   const [phoneNumberError, setPhoneNumberError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-  
   const navigate = useNavigate();
 
-  // const toggleNewPasswordVisibility = () => {
-  //   setShowNewPassword(!showNewPassword);
-  // };
+  const toggleNewPasswordVisibility = () => {
+    setShowNewPassword(!showNewPassword);
+  };
 
-  // const toggleConfirmPasswordVisibility = () => {
-  //   setShowConfirmPassword(!showConfirmPassword);
-  // };
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
+
     // Validation logic for each field
-    if (!username) {
-      setUsernameError('Username is required');
+    if (!firstName) {
+      setFirstNameError('First name is required');
       return;
     }
-    if (!email) {
-      setEmailError('Email is required');
-      return;
-    }
-    if (!company) {
-      setCompanyError('Organization is required');
+    if (!lastName) {
+      setLastNameError('Last name is required');
       return;
     }
     if (!phoneNumber) {
@@ -56,23 +48,23 @@ function Register() {
       return;
     }
     if (!newPassword) {
-      setPasswordError('New password is required');
+      setPasswordError('Password is required');
       return;
     }
+    
     if (newPassword !== confirmPassword) {
       setPasswordError('Passwords do not match');
       return;
     }
-  
+
     // If all validations pass, proceed with form submission
     const requestBody = {
-      userName: username,
+      firstName: firstName,
+      lastName: lastName,
       password: newPassword,
-      email: email,
-      organization: company,
       phoneNo: phoneNumber,
     };
-  
+
     try {
       const response = await fetch(`${PortURL}/register`, {
         method: 'POST',
@@ -81,26 +73,22 @@ function Register() {
         },
         body: JSON.stringify(requestBody),
       });
-  
+
       if (response.ok) {
         console.log('User registered successfully!');
         navigate('/login');
       } else {
         const data = await response.json();
         console.error('Error registering user:', data.message);
-        
-          if (response.status === 400) {
+        if (response.status === 400) {
           if (data.errors) {
-            if (data.errors.userName) {
-              setUsernameError(data.errors.userName);
+            if (data.errors.firstName) {
+              setFirstNameError(data.errors.firstName);
             }
-            if (data.errors.email) {
-              setEmailError(data.errors.email);
+            if (data.errors.lastName) {
+              setLastNameError(data.errors.lastName);
             }
-            if (data.errors.organization) {
-              setCompanyError(data.errors.organization);
-            }
-            if (data.errors.phoneNo) {  
+            if (data.errors.phoneNo) {
               setPhoneNumberError(data.errors.phoneNo);
             }
             if (data.errors.password) {
@@ -117,136 +105,137 @@ function Register() {
   };
 
   return (
-   <div>
-    <Header/>
-     <Container className="mt-5 shadow p-3 mb-5 bg-body container vh-70">
-        <h6 className="text-center mb-3 mt-2 display-6 ">Sign up</h6>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formFirstName" className="mb-3">
-          <TextField
-            className="label"
-            type="text"
-            label="Username"
-            value={username}
-            onChange={(e) => {
-              setUserName(e.target.value);
-              setUsernameError('');
-            }}
-            fullWidth
-            size="small"
-            error={!!usernameError}
-            helperText={usernameError}
-            
-          />
-        </Form.Group>
-        
-        <Form.Group controlId="formEmailAddress" className="mb-3">
-          <TextField
-            className="label"
-            type="email"
-            label="Email address"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              setEmailError('');
-            }}
-            fullWidth
-            size="small"
-            error={!!emailError}
-            helperText={emailError}
-          />
-        </Form.Group>
-        
-        <Form.Group controlId="formCompany" className="mb-3">
-          <TextField
-            className="label"
-            type="text"
-            label="Organization"
-            value={company}
-            onChange={(e) => {
-              setCompany(e.target.value);
-              setCompanyError('');
-            }}
-            fullWidth
-            size="small"
-            error={!!companyError}
-            helperText={companyError}
-          />
-        </Form.Group>
-        
-        <Form.Group controlId="formPhoneNumber" className="mb-3">
-  <TextField
-    className="label"
-    type="tel"
-    label="Mobile number"
-    value={phoneNumber}
-    onChange={(e) => {
-      const enteredValue = e.target.value.replace(/\D/g, '');
-      setPhoneNumber(enteredValue);
-      setPhoneNumberError('');
-    }}
-    fullWidth
-    size="small"
-    error={!!phoneNumberError}
-    helperText={phoneNumberError}
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">+91</InputAdornment>
-      ),
-      inputMode: 'numeric', 
-    }}
-  />
-</Form.Group>
-
-
-        
-        <Form.Group controlId="formNewPassword" className="mb-3">
-          <TextField
-            className="label"
-            type={showNewPassword ? 'text' : 'password'}
-            label="New password"
-            value={newPassword}
-            onChange={(e) => {
-              setNewPassword(e.target.value);
-              setPasswordError('');
-            }}
-            fullWidth
-            size="small"
-            error={!!passwordError}
-            helperText={passwordError}
-            
-          />
-        </Form.Group>
-        
-        <Form.Group controlId="formConfirmPassword" className="mb-3">
-          <TextField
-            className="label"
-            type={showConfirmPassword ? 'text' : 'password'}
-            label="Confirm password"
-            value={confirmPassword}
-            onChange={(e) => {
-              setConfirmPassword(e.target.value);
-              setPasswordError('');
-            }}
-            fullWidth
-            size="small"
-            error={!!passwordError}
-            helperText={passwordError}
-            
-          />
-        </Form.Group>
-        
-        <div className="btn-container mt-5 mb-4">
-          <Button type="submit" className="btn btn-success submit rounded-pill">
-            Submit
-          </Button>
-        </div>
-      </Form>
-    </Container>
-   </div>
+    <div>
+      <Header/>
+      <Container className="mt-5 shadow p-3 mb-5 bg-body container vh-70">
+        <Form onSubmit={handleSubmit}>
+          <h6 className="text-center mb-5 mt-2 display-6 ">Sign up</h6>  
+          <Row className="row-cols-1 row-cols-md-2">
+            <Col>
+              <Form.Group controlId="formFirstName" className="mb-4">
+                <TextField
+                  className="label"
+                  type="text"
+                  label="First Name"
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                    setFirstNameError('');
+                  }}
+                  fullWidth
+                  size="small"
+                  error={!!firstNameError}
+                  helperText={firstNameError}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="formLastName" className="mb-4">
+                <TextField
+                  className="label"
+                  type="text"
+                  label="Last Name"
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                    setLastNameError('');
+                  }}
+                  fullWidth
+                  size="small"
+                  error={!!lastNameError}
+                  helperText={lastNameError}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Form.Group controlId="formPhoneNumber" className="mb-4">
+            <TextField
+              className="label"
+              type="tel"
+              label="Mobile number"
+              value={phoneNumber}
+              onChange={(e) => {
+                const enteredValue = e.target.value.replace(/\D/g, '');
+                setPhoneNumber(enteredValue);
+                setPhoneNumberError('');
+              }}
+              fullWidth
+              size="small"
+              error={!!phoneNumberError}
+              helperText={phoneNumberError}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">+91</InputAdornment>
+                ),
+                inputMode: 'numeric',
+              }}
+            />
+          </Form.Group>
+          <Form.Group controlId="formNewPassword" className="mb-4">
+            <TextField
+              className="label"
+              type={showNewPassword ? 'text' : 'password'}
+              label="New password"
+              value={newPassword}
+              onChange={(e) => {
+                setNewPassword(e.target.value);
+                setPasswordError('');
+              }}
+              fullWidth
+              size="small"
+              error={!!passwordError}
+              helperText={passwordError}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    {showNewPassword ? (
+                      <VisibilityIcon onClick={toggleNewPasswordVisibility} />
+                    ) : (
+                      <VisibilityOffIcon onClick={toggleNewPasswordVisibility} />
+                    )}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Form.Group>
+          <Form.Group controlId="formConfirmPassword" className="mb-4">
+            <TextField
+              className="label"
+              type={showConfirmPassword ? 'text' : 'password'}
+              label="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+                setPasswordError('');
+              }}
+              fullWidth
+              size="small"
+              error={!!passwordError}
+              helperText={passwordError}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    {showConfirmPassword ? (
+                      <VisibilityIcon onClick={toggleConfirmPasswordVisibility} />
+                    ) : (
+                      <VisibilityOffIcon onClick={toggleConfirmPasswordVisibility} />
+                    )}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Form.Group>
+          <div className="btn-container mt-5 mb-2">
+            <Button type="submit" className="btn btn-success  rounded-pill">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Container>
+    </div>
   );
 }
 
 export default Register;
 
-
+             
