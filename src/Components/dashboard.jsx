4b86de15609cabfa09a554b95118cfa5122bd315
Siva-bharathiP <136304492+Siva-bharathiP@users.noreply.github.com>
2024-04-01 12:21:33 +0530

    Excel heading sticky

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -544,92 +544,93 @@ const formatDateCell = (value, key) => {
 {filteredData.length === 0 ? (
         <div className="no-data-message">No data available</div>
       ) :
-        <Container fluid className="mt-2">
-        <Row className="row Render-Row">
-          <Col className="col Render-Col">
-            <div className="table-responsive render">
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th className="selection-cell">
+      <Container fluid className="mt-2">
+  <Row className="row Render-Row">
+    <Col className="col Render-Col">
+      <div className="table-responsive render">
+        <Table striped bordered hover>
+          <thead className="sticky-header">
+            <tr>
+              <th className="selection-cell">
+                <input
+                  type="checkbox"
+                  checked={selectedRowIds.length === filteredData.length}
+                  onChange={() => handleCheckboxChange(null)}
+                />
+              </th>
+              {Object.keys(filteredData[0] || {}).map((key) => (
+                <th key={key}>
+                  {key === 'MonthYear' ? 'Date' : key}
+                </th>
+              ))}
+              <th className="action-cell">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((row, index) => (
+              <tr key={index}>
+                <td className="selection-cell">
+                  <input
+                    type="checkbox"
+                    checked={selectedRowIds.includes(index)}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+                </td>
+                {Object.keys(row).map((key) => (
+                  <td key={key}>
+                    {editedRowId === index ? (
                       <input
-                        type="checkbox"
-                        checked={selectedRowIds.length === filteredData.length}
-                        onChange={() => handleCheckboxChange(null)}
+                        type="text"
+                        value={editedRowData[key] || ""}
+                        onChange={(e) => handleInputChange(e, key)}
                       />
-                    </th>
-                    {Object.keys(filteredData[0] || {}).map((key) => (
-                      <th key={key}>
-                        {key === 'MonthYear' ? 'Date' : key}
-                      </th>
-                    ))}
-                    <th className="action-cell">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {filteredData.map((row, index) => (
-                    <tr key={index}>
-                      <td className="selection-cell">
-                        <input
-                          type="checkbox"
-                          checked={selectedRowIds.includes(index)}
-                          onChange={() => handleCheckboxChange(index)}
-                        />
-                      </td>
-                      {Object.keys(row).map((key) => (
-                        <td key={key}>
-                          {editedRowId === index ? (
-                            <input
-                              type="text"
-                              value={editedRowData[key] || ""}
-                              onChange={(e) => handleInputChange(e, key)}
-                            />
-                          ) : (
-                            formatDateCell(row[key], key)
-                          )}
-                        </td>
-                      ))}
-                      <td className="action-cell">
-                        {editedRowId === index ? (
-                          <div className="action-buttons">
-                            <button
-                              className="btn  btn-sm Save"
-                              onClick={() => handleSave()}
-                            >
-                              <FontAwesomeIcon icon={faSave} />
-                            </button>
-                            <button
-                              className="btn btn-sm Cancel"
-                              onClick={() => handleCancel()}
-                            >
-                              <FontAwesomeIcon icon={faTimes} />
-                            </button>
-                          </div>
-                        ) : (
-                          <div className="action-buttons">
-                            <button
-                              className="btn  btn-sm Edit"
-                              onClick={() => handleEdit(index)}
-                            >
-                              <FontAwesomeIcon icon={faEdit} />
-                            </button>
-                            <button
-                              className="btn btn-sm Delete"
-                              onClick={() => handleDelete(index)}
-                            >
-                              <FontAwesomeIcon icon={faTrash} />
-                            </button>
-                          </div>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+                    ) : (
+                      formatDateCell(row[key], key)
+                    )}
+                  </td>
+                ))}
+                <td className="action-cell">
+                  {editedRowId === index ? (
+                    <div className="action-buttons">
+                      <button
+                        className="btn  btn-sm Save"
+                        onClick={() => handleSave()}
+                      >
+                        <FontAwesomeIcon icon={faSave} />
+                      </button>
+                      <button
+                        className="btn btn-sm Cancel"
+                        onClick={() => handleCancel()}
+                      >
+                        <FontAwesomeIcon icon={faTimes} />
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="action-buttons">
+                      <button
+                        className="btn  btn-sm Edit"
+                        onClick={() => handleEdit(index)}
+                      >
+                        <FontAwesomeIcon icon={faEdit} />
+                      </button>
+                      <button
+                        className="btn btn-sm Delete"
+                        onClick={() => handleDelete(index)}
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </div>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </Col>
+  </Row>
+</Container>
+
     
   }
         {loading && <LoadingSpinner />} 
