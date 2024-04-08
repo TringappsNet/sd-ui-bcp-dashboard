78fdb099380dac014bac90a6ae4a40161a2d6d83
JohnsonMelvin <jmelvinmelu113@gmail.com>
2024-04-08 13:18:55 +0530

    Changes for bulk update-upload and object mapper

---
+++ src/Objects/Objects.js
@@ -0,0 +1,33 @@
+const columnMap = {
+    "Month/Year": "MonthYear",
+    "Company Name": "CompanyName",
+    "Revenue Actual": "RevenueActual",
+    "Revenue Budget": "RevenueBudget",
+    "Gross Profit Actual": "GrossProfitActual",
+    "Gross Profit Budget": "GrossProfitBudget",
+    "SG & A Actual": "SGAActual",
+    "SG & A Budget": "SGABudget",
+    "EBITDA Actual": "EBITDAActual",
+    "EBITDA Budget": "EBITDABudget",
+    "CapEx Actual": "CapExActual",
+    "CapEx Budget": "CapExBudget",
+    "Fixed Assets (Net) Actual": "FixedAssetsNetActual",
+    "Fixed Assets (Net) Budget": "FixedAssetsNetBudget",
+    "Cash Actual": "CashActual",
+    "Cash Budget": "CashBudget",
+    "Total Debt Actual": "TotalDebtActual",
+    "Total Debt Budget": "TotalDebtBudget",
+    "Accounts Receivable Actual": "AccountsReceivableActual",
+    "Accounts Receivable Budget": "AccountsReceivableBudget",
+    "Accounts Payable Actual": "AccountsPayableActual",
+    "Accounts Payable Budget": "AccountsPayableBudget",
+    "Inventory Actual": "InventoryActual",
+    "Inventory Budget": "InventoryBudget",
+    "Employees Actual": "EmployeesActual",
+    "Employees Budget": "EmployeesBudget",
+    "Quarter": "Quarter"
+  };
+
+
+  export default columnMap;
+
