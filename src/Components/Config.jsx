f3f6f914fef3a8f188858c6c388501a5995ae7a5
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-06-20 19:41:22 +0530

    Main config

--- src/Components/Config.jsx
+++ src/Components/Config.jsx
@@ -1,10 +1,1 @@
-// export const PortURL = 'http://14.194.141.107:3002'; 
-
-// export const PortURL = 'http://192.168.1.50:3001';
-
-// export const PortURL = 'http://192.168.1.225:3001';
-
-// export const PortURL = 'http://localhost:3001';
-
-
-export const PortURL = 'http://18.219.123.60/api';
+export const PortURL = 'https://bcpbackend.azurewebsites.net/api';
