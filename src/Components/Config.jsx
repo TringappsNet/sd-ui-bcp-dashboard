7e07f226ca804a1b128dff986993de3695a967b7
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-07-12 15:42:37 +0530

    bcp azure master branch

--- src/Components/Config.jsx
+++ src/Components/Config.jsx
@@ -1,10 +1,4 @@
-// export const PortURL = 'http://14.194.141.107:3002'; 
+export const PortURL = 'https://bcpbackend.azurewebsites.net/api';
 
-// export const PortURL = 'http://192.168.1.50:3001';
+// export const PortURL = 'http://localhost:3001/api';
 
-// export const PortURL = 'http://192.168.1.225:3001';
-
-// export const PortURL = 'http://localhost:3001';
-
-
-export const PortURL = 'http://18.219.123.60/api';
